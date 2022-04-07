afcdd6691ee2bd6cd1f682ac2459343b69d41155
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-07 11:24:31 +0200

    api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  return element;
+const personData = [
+  { name: 'regiss', age: 31},
+  { name: 'allain', age: 30 },
+  { name: 'papa', age: 70 },
+  { name: 'Lualua', age: 210 },
+  { name: 'Just', age: 210 },
+  { name: 'Franc', age: 210 },
+  { name: 'Gigi', age: 210 },
+];
+
+function loadTableData(personData) {
+  const tableBody = document.getElementById('tableData');
+  // const table = document.createElement('div');
+  let dataHtml = '';
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const person of personData) {
+    dataHtml += `<tr><td>${person.name}</td><td>${person.age}</td></tr>`;
+  }
+  tableBody.innerHTML = dataHtml;
 }
 
-document.body.appendChild(component());
+window.onload = () => {
+  loadTableData(personData);
+};
+
+loadTableData(personData);
